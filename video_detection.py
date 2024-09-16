71d49e99529ed38edb537271941c7666ea1f41a9
Marcio <marcio.de.carvalho@tecnico.ulisboa.pt>
2024-09-16 17:29:45 +0100

    added convert_templates in video_detection.py

--- video_detection.py
+++ video_detection.py
@@ -4,18 +4,23 @@ import numpy as np
 templates = []
 templates_shapes = []
 templates_grey = []
+convert_templates = {} #gives each template an int value, the templates are the keys, and each one of them as a number as value
 
 #guarda todos os templates numa lista
 for i in range(0, 9):
-    templates.append(cv2.imread(f'template{i + 1}.png'))
+    templates.append(cv2.imread(f'images/template{i + 1}.png'))
     templates_shapes.append(templates[i].shape[:2])
     templates_grey.append(cv2.cvtColor(templates[i], cv2.COLOR_BGR2GRAY))
+
+    #adds a template and its respective
+    convert_templates[f'images/template{i + 1}.png'] = i + 1 
     
 
 #adds the whitespace template
-templates.append(cv2.imread('whitespace1.png'))
+templates.append(cv2.imread('images/whitespace1.png'))
 templates_shapes.append(templates[9].shape[:2])
 templates_grey.append(cv2.cvtColor(templates[9], cv2.COLOR_BGR2GRAY))
+convert_templates['images/whitespace1.png'] = 0
 
 #get the video stream input
 video = cv2.VideoCapture('http://192.168.1.246:8080/video')
@@ -28,7 +33,9 @@ else:
     height = int(video.get(4))
 
     while (video.isOpened()):
+
         ret, frame = video.read()
+
         if ret:
             imagem = frame.copy()
             imagem_grey = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
@@ -39,11 +46,10 @@ else:
                 resultado = cv2.matchTemplate(imagem_grey, templates_grey[comparisons], cv2.TM_CCOEFF_NORMED)
                 location = np.where(resultado >= threshold)
                 altura, largura = templates_shapes[comparisons] #gives us the height and width of the given template to do
+
                 for i in zip(*location[::-1]):
                     cv2.rectangle(imagem, i, (i[0] + altura, i[1] + largura), (0, 0, 255), 2)
                     cv2.imshow('result', frame)
-                    
-
 
         else:
             break
